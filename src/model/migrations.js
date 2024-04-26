8f32522a9b523402f0a5e335061e262cf29323d8
Damian <damianpietka@gmail.com>
2024-04-27 01:37:15 +0200

    added database migration

--- src/model/migrations.js
+++ src/model/migrations.js
@@ -1,7 +1,19 @@
-import { schemaMigrations } from '@nozbe/watermelondb/Schema/migrations';
+import {
+  schemaMigrations,
+  addColumns,
+} from '@nozbe/watermelondb/Schema/migrations';
 
 export default schemaMigrations({
   migrations: [
     // We'll add migration definitions here later
+    {
+      toVersion: 2,
+      steps: [
+        addColumns({
+          table: 'categories',
+          columns: [{ name: 'created_at', type: 'number' }],
+        }),
+      ],
+    },
   ],
 });
